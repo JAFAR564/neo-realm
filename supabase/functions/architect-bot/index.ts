d865bbea17f212de5c6c017d940709018180c543
Phenix <krielfenance@gmail.com>
2025-09-23 16:43:13 +0200

    Update Deno imports in Supabase functions

--- supabase/functions/architect-bot/index.ts
+++ supabase/functions/architect-bot/index.ts
@@ -1,5 +1,5 @@
 // Architect Bot - Simple bot that sends periodic system messages
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // Initialize Supabase client
